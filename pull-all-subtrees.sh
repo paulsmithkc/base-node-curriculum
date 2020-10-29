99a76761a9dc3d76d594a023f61ff89ad30c332e
Paul Smith <prsmith@ranken.edu>
2020-10-29 15:13:40 -0500

    moving mongodb example to unit 4

--- pull-all-subtrees.sh
+++ pull-all-subtrees.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git subtree pull -P unit_01/examples/example_express example-express master --squash
 git subtree pull -P unit_02/examples/notepad_mysql notepad-mysql master --squash
-git subtree pull -P unit_03/examples/notepad_mongodb notepad-mongodb master --squash
+git subtree pull -P unit_04/examples/notepad_mongodb notepad-mongodb master --squash
 git subtree pull -P unit_05/examples/notepad_mongodb_react notepad-react master --squash
 
